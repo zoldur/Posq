58a02e5dd57854df785fbcfe599458bf4b79859b
Zoldur <zoldur99@gmail.com>
2018-06-16 03:19:47 -0700

    Get IP

--- posq_install.sh
+++ posq_install.sh
@@ -11,7 +11,7 @@ COIN_NAME='Poseidon'
 COIN_PORT=5510
 RPC_PORT=5511
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -158,7 +158,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
